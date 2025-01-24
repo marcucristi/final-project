757757136e6900771a181ff5ccd7988af06318bf
marcucristi <marcu.cristi19@gmail.com>
2025-01-24 18:43:27 +0200

    user and balance logic

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-//    public static void main(String[] args) {
-//        BalanceLogic user1 = new BalanceLogic(100.00);
-//        System.out.println("Balans initial " + user1.getBalance());
-//
-//        user1.addBalance(9.12);
-//        System.out.println();
-//        user1.addBalance(98.21);
-//        System.out.println();
-//
-//        user1.withdrawBalance(30);
-//        System.out.println();
-//        user1.withdrawBalance(5.50);
-//        System.out.println();
-//        user1.withdrawBalance(951);
-//        System.out.println();
-//
-//
-//
-//    }
+    public static void main(String[] args) {
+        try {
+            User user = new User("Ion Verejan", "Unde e bine", 22, 50.0);
+            user.displayUserInfo();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Atentie: " + e.getMessage());
+        }
+    }
 }
